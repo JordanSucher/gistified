5976f6de10f0abb7cdb018b948d94a73fe92caee
JordanSucher <jsucher@gmail.com>
2023-11-26 19:27:02 -0500

    trying jobs

--- jobs/processNewPodcast.js
+++ jobs/processNewPodcast.js
@@ -18,9 +18,9 @@ client.defineJob({
         const { rssFeedUrl } = payload;
 
         // Use getLatestEpisodes to get a list of 3 MP3 URLs
-        // const episodes = await getLatestEpisodes(rssFeedUrl);
+        const episodes = await getLatestEpisodes(rssFeedUrl);
         // const mp3Urls = episodes.map(episode => episode.url);
-        const episodes = [{url: 'https://dcs.megaphone.fm/NPR3473178103.mp3?e=1197954683&key=aba2f830cbe3f90625553d4d668fe70f&p=510289&request_event_id=5b30dc05-9d40-4d69-a257-21c8688b621d&size=25442056&t=podcast', title: 'Planet Money Episode', description: 'Test description', pubDate: '2022-06-08T00:00:00+00:00'}];
+        // const episodes = [{url: 'https://dcs.megaphone.fm/NPR3473178103.mp3?e=1197954683&key=aba2f830cbe3f90625553d4d668fe70f&p=510289&request_event_id=5b30dc05-9d40-4d69-a257-21c8688b621d&size=25442056&t=podcast', title: 'Planet Money Episode', description: 'Test description', pubDate: '2022-06-08T00:00:00+00:00'}];
 
         // Process each MP3 URL
         for (const episode of episodes) {
