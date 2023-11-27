16c7aa8726fc470fa454570e7c3cdeac5369efec
JordanSucher <jsucher@gmail.com>
2023-11-26 19:12:34 -0500

    adding user specific filtering to summaries page

--- app/summaries/page.js
+++ app/summaries/page.js
@@ -1,8 +1,17 @@
 import SummaryCard from "../ui/Summaries/SummaryCard"
 import prisma from '../prisma'
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/authOptions"
+
+
 export default async function Summaries () {
 
-      let summaries = await prisma.summary.findMany({
+    let session = await getServerSession(authOptions)
+
+      let summaries = session ? await prisma.summary.findMany({
+        where: {
+            userId: session.user.id
+        },
         include: {
             // publication: true,
             episode: {
@@ -11,7 +20,7 @@ export default async function Summaries () {
                 }
             }
         }
-        })
+        }) : []
     
     return (
         <div className='w-full h-full max-w-[1000px] self-center'>
